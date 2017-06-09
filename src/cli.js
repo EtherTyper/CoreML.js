04391630b177b3797a9394bc049d71e8f522e35c
Eli Joesph Bradley <elijos03@gmail.com>
2017-06-08 20:09:58 -0500

    Add all the basic metadata.

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import * as utilities from '.'
+import { Model } from '.'
 
 if (!(process.argv.length >= 4)) {
   throw new TypeError("Missing parameters.");
@@ -9,13 +9,29 @@ if (!(process.argv.length >= 4)) {
 const commandParam = process.argv[2];
 const fileParam = process.argv[3];
 
-const model = utilities.loadFile(fileParam);
+const model = new Model(fileParam);
 
 switch (commandParam) {
   case "decode":
-    console.log(JSON.stringify(model));
+    console.log(JSON.stringify(model.object));
+    break;
+  case "name":
+    console.log(model.name);
+    break;
+  case "type":
+    console.log(model.type);
+    break;
+  case "size":
+    console.log(`${model.size} bytes`);
+    break;
+  case "author":
+    console.log(model.author);
     break;
   case "license":
+    console.log(model.license);
+    break;
+  case "description":
+    console.log(model.description);
     break;
   default:
     throw new TypeError("Invalid command.");
--- src/index.js
+++ src/index.js
@@ -1,11 +1,59 @@
-import {Model} from './messages/Model_pb';
+import { Model as ModelMessage } from './messages/Model_pb';
+import path from 'path';
 import fs from 'fs';
 
-export function loadFile(fileName) {
-  const loadedFile = fs.readFileSync(fileName);
-  return Model.deserializeBinary(loadedFile);
-}
+// String value describing model type isn't available, however
+// you can get the all caps name of the type from the object
+// ModelMessage.TypeCase (Which is { [type: String]: number })
+export const typeNames = Object
+  .keys(ModelMessage.TypeCase)
+  .reduce(function (typeNames, name) {
+    typeNames[ModelMessage.TypeCase[name]] = name; // Reverse key value pair order.
+    return typeNames;
+  }, {});
 
-export function getDescription(model) {
+export class Model {
+  constructor(fileName) {
+    this.fileName = fileName;
+    this.model = this.loadFile(fileName);
+  }
   
+  loadFile(fileName) {
+    this.loadedFile = fs.readFileSync(fileName);
+    return ModelMessage.deserializeBinary(this.loadedFile);
+  }
+  
+  get object() {
+    return this.model.toObject();
+  }
+  
+  get metadata() {
+    const description = this.model.getDescription();
+    return description.getMetadata();
+  }
+  
+  get name() {
+    return path.basename(this.fileName, '.mlmodel');
+  }
+  
+  get type() {
+    const typeCase = this.model.getTypeCase();
+    return typeNames[typeCase];
+  }
+  
+  get size() {
+    return this.loadedFile.byteLength;
+  }
+  
+  get author() {
+    return this.metadata.getAuthor();
+  }
+  
+  get license() {
+    return this.metadata.getLicense();
+  }
+  
+  get description() {
+    return this.metadata.getShortdescription();
+  }
 }
